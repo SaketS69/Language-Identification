bca2f00623aa9af97de921d5db840030e30d7ddc
SaketS69 <saket7893@gmail.com>
2024-11-20 23:18:41 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+
+setup(name="Language_Identification_Using_CNN",
+    author_name="Saket",
+    author_email="sumansaket197@gmail.com",
+    packages=find_packages(),
+    install_requires=[])
